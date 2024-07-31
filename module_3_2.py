83e2a8d8ce7bd609e6e66b10e03831414060a57c
1e7mecry <158025119+1e7mecry@users.noreply.github.com>
2024-07-31 13:43:55 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,38 @@
+def send_email(massage, recipient, sender="university.help@gmail.com"):
+    domains = [".com", ".ru", ".net"]
+    check_progression = True
+    while True:
+        if "@" not in sender or "@" not in recipient:
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+            check_progression = False
+            break
+        else:
+            if sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net'):
+                if recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net'):
+                    break
+                else:
+                    print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+                    check_progression = False
+                    break
+            else:
+                print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+                check_progression = False
+                break
+    while check_progression:
+        if recipient == sender:
+            print("Нельзя отправить письмо самому себе!")
+            break
+        if sender == 'university.help@gmail.com':
+            print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+            break
+        else:
+            print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+            break
+
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
